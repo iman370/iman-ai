c2b3966753cb0d309dca6f4660552497a584fdc4
iman370 <imaniqbal@live.co.uk>
2022-08-13 23:53:01 +0100

    Updated names and some meta data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div id="website-container">
       <div className="landing-container">
-        <h1>iman.ai()</h1>
+        <h1>iman-ai.tech()</h1>
       </div>
       <div className="about-container">
         <h1>Website In Progress</h1>
