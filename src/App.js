4a61a96658f77ffa1600c8aa50fdbdab4b9a4a6b
iman370 <imaniqbal@live.co.uk>
2022-08-13 21:25:40 +0100

    Changed background and added 2 sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Testing
-        </a>
-      </header>
+    <div id="website-container">
+      <div className="introduction-container">
+        <h1>iman.ai()</h1>
+      </div>
+      <div className="about-container">
+        <h1>aaaaaa</h1>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 }
+
 
 export default App;
