2c4c5ff625b00d68ad141b6b688ea03ad22a1775
iman370 <imaniqbal@live.co.uk>
2022-08-14 18:35:47 +0100

    Changed font size

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,7 @@ import { motion, useScroll, useTransform, AnimatePresence } from "framer-motion"
 
 function Landing() {
     const { scrollYProgress } = useScroll({offset: ["end end", "start start"]});
-    const scale = useTransform(scrollYProgress, [0, 5], [0.2, 10]);
+    const scale = useTransform(scrollYProgress, [0, 9], [0.2, 10]);
     const [isVisible] = useState(true);
 
     return(
