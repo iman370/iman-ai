b5dfa3db4ed479e702e81bb0a837b571436cb38f
iman370 <imaniqbal@live.co.uk>
2022-08-14 23:01:07 +0100

    Added new animations

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 //import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
+
 function App() {
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const FadeUp = batch(Fade(), Move(), Sticky());
 
   return (
     <div id="website-container">
+      <ScrollContainer>
       <NavBar />
-      <Landing />
-      <About />
-      <Projects />
-      
-      <Footer />
+        <Animator animation={batch(Sticky(), MoveOut(0, -200))}>
+          <Landing />
+        </Animator>
+        <ScrollPage>
+          <Animator animation={batch(Fade(), Sticky(), MoveIn(-1000, 0))}>
+            <About />
+          </Animator>
+        </ScrollPage>
+        <ScrollPage>
+          <Animator animation={FadeUp}>
+            <Projects />
+          </Animator>
+        </ScrollPage>
+        <ScrollPage>
+          <Animator animation={FadeUp}>
+            <Footer />
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
     </div>
   );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,23 +2,16 @@ import React, { useState } from 'react';
 import { motion, useScroll, useTransform, AnimatePresence } from "framer-motion";
 
 function Landing() {
-    const { scrollYProgress } = useScroll({offset: ["end end", "start start"]});
-    const scale = useTransform(scrollYProgress, [0, 9], [0.2, 10]);
-    const [isVisible] = useState(true);
+    //const { scrollYProgress } = useScroll({offset: ["end end", "start start"]});
+    //const scale = useTransform(scrollYProgress, [0, 9], [0.2, 10]);
+    //const [isVisible] = useState(true);
 
     return(
         <div className="landing-container">
-            <AnimatePresence>
-                {isVisible && (
-                <motion.div
-                exit={{y:-1000}}
-                style={{ scale }}>
-                    <div className='typewriter'>
-                        <h1>iman-ai.tech()</h1>
-                        <h2>Computer Science Student</h2>
-                    </div>
-                </motion.div>)}
-            </AnimatePresence>
+            <div className='typewriter'>
+                <h1>iman-ai.tech()</h1>
+                <h2>Computer Science Student</h2>
+            </div>
         </div>
     );
 }
