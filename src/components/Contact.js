e4015b84c14ce2fd4d6a44dcc67476f99628e1de
iman370 <imaniqbal@live.co.uk>
2022-08-15 18:10:27 +0100

    Added github link+logo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import githubLogo from './images/github-logo.png';
 import { Animator, ScrollPage, batch, FadeIn, Move, Sticky } from "react-scroll-motion";
 
 function Contact() {
@@ -9,6 +10,11 @@ function Contact() {
             <div className='header'>
                 <h1>iman-ai<b>.links()</b></h1>
               <div className='divider' />
+            </div>
+            <div className='logos-tray'>
+              <a href="https://github.com/iman370">
+                <img className="logos" src={githubLogo} alt="GitHub"/>
+              </a>
             </div>
         </Animator>
       </ScrollPage>
