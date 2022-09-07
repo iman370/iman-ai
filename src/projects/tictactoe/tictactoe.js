205deed83f6e9ca5891a962c10af1baeb3923dae
iman370 <imaniqbal@live.co.uk>
2022-09-07 20:54:28 +0100

    Worked on the tictactoe page

--- src/projects/tictactoe/tictactoe.js
+++ src/projects/tictactoe/tictactoe.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import { Button } from '@mui/material';
 
-function tictactoe() {
+function Tictactoe() {
+    const [board] = useState(['X','X','X','X','X','X','X','X','X'])
+
     return(
-        <p>aaaaa</p>
+        <div id="tictactoe-container">
+            <Toolbar>
+                <div className='navbar'>
+                    <div className='logo'>
+                        <a href='../'>
+                            <p>iman-ai.tech()</p>
+                        </a>
+                    </div>
+                </div>
+            </Toolbar>
+            <div id="content">
+                <div id="column1">
+                    <Button className='back-button' href='../'>🠔 Back</Button>
+                </div>
+                <div id="column2">
+                    <div className='heading'>
+                        <div className='typewriter'>
+                            <h1>tictactoe using minimax</h1>
+                        </div>
+                    </div>
+                    <div className='divider-small-vh' />
+                    <div id="board-container">
+                        {board.map(function(square){
+                            return(
+                                <p>{square}</p>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default tictactoe
+export default Tictactoe
