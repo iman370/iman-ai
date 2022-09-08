5f506ee5e34803b6b535eead3e59099adb67f02f
iman370 <imaniqbal@live.co.uk>
2022-09-08 21:25:12 +0100

    Reworking about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import imanpic from './images/imanpic.png'
 
 //import { Animator, ScrollPage, batch, Fade, Move, Sticky} from "react-scroll-motion";
 
@@ -7,17 +8,15 @@ function About() {
     return (
         <div className="section">
         <div className='about-container'>
-            <div className='header'>
-                <h1>iman-ai<b>.about()</b></h1>
-            </div>
-            <div className='divider' />
-            <div className='about-me'>
-                <p>Hi, <b>Iman</b> here!</p>
-                <div className='divider-small' />
-                <p>I am currently a third year student studying a bachelors in <b>Computer Science</b> at the <b>University of Birmingham</b>.</p>
-                <p>I am heavily interested in software engineering, full-stack development (mainly back-end development) and AI/ML.</p>
-                <div className='divider-small' />
-                <p>Outside of work, I love to spend my time programming a new project or learning something new. I also enjoy skateboarding and playing video games.</p>
+            <div id='about-content'>
+                <div className='column1'>
+                    <div className='divider-smaller-vh' />
+                    <img src={imanpic} className="aboutmeImage" alt="Iman in Scotland" />
+                    <p>Outside of work, I love to go skateboarding and play video games! My favourite game is Assassins Creed Syndicate. I'm a huge fan of Industrial and Victorian England.</p>
+                </div>
+                <div className='column2'>
+                    <p>Hi, <b>Iman</b> here!</p>
+                </div>
             </div>
         </div>
         </div>
