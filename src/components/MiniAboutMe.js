4c30aa9fecf5b025b43698a0cb13770fd707d8e3
iman370 <imaniqbal@live.co.uk>
2022-09-24 19:00:15 +0100

    Added the move in animation

--- src/components/MiniAboutMe.js
+++ src/components/MiniAboutMe.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Animator, MoveIn,} from "react-scroll-motion";
 
 function MiniAboutMe() {
     return(
         <div id="miniaboutme-content">
-            <p>Diligent and devoted to creating imaginative software. I take pride in providing clean and efficient code.</p>
+            <span className="h1_text">
+                <Animator animation={MoveIn(-1000, 0)}>Diligent and devoted to creating imaginative software.</Animator>
+                <Animator animation={MoveIn(1000, 0)}>I take pride in providing clean and efficient code.</Animator>
+            </span>
         </div>
     )
 }
