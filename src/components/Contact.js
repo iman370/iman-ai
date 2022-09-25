f37b3eb8e01835fda3429bce9ab345e1b5658d43
iman370 <imaniqbal@live.co.uk>
2022-09-25 18:04:23 +0100

    Added kaggle link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import githubLogo from './images/github-logo.png';
 import linkedInLogo from './images/linkedin-logo.png';
+import kaggleLogo from './images/kaggle-logo.webp';
 //import { Animator, ScrollPage, batch, FadeIn, Move, Sticky } from "react-scroll-motion";
 
 function Contact() {
@@ -17,6 +18,10 @@ function Contact() {
         <div className='divider-width' />
         <a href="https://www.linkedin.com/in/iman-iqbal-7133851b3">
           <img className="logos" src={linkedInLogo} alt="LinkedIn logo"/>
+        </a>
+        <div className='divider-width' />
+        <a href="https://www.kaggle.com/iman1733">
+          <img className="logos" src={kaggleLogo} alt="Kaggle logo"/>
         </a>
       </div>
     </div>
