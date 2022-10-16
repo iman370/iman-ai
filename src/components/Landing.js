c7c7478535094ef571ef5d14d5853a9bf280469c
iman370 <imaniqbal@live.co.uk>
2022-10-16 18:16:50 +0100

    Remaking landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import rikkagif from './images/rikka.gif';
 
 //import { Animator, ScrollPage, batch, MoveOut, Sticky, Fade} from "react-scroll-motion";
 
 function Landing() {
     return(
-      <div className="section">
-        <div className="landing-container">
+      <div className="landing-container">
+        <div className='column1'>
+        <img className="picture" src={rikkagif} alt="Anime gif" />
+        </div>
+        <div className='column2'>
+          <div className='heading'>
             <div className='typewriter'>
                 <h1>iman-ai.tech()</h1>
-                <h2>Computer Science Student</h2>
             </div>
+          </div>
+          <h2>Computer Science Student</h2>
         </div>
       </div>
     );
