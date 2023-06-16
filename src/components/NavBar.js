5c02801851fcfc4cf6f953add1d78c84be6acc7b
iman370 <imaniqbal@live.co.uk>
2023-06-16 20:11:05 +0100

    Changed navbar to bottom right and looks cleaner

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,23 @@
 import * as React from 'react';
 //import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
+//import Toolbar from '@mui/material/Toolbar';
 import { Link } from 'react-scroll';
 
 function NavBar() {
   return (
-    <div className="navbar">
-        <Toolbar>
-          <div className='logo'>
-            <Link activeClass="active" className="links" to="landing-container" spy smooth duration={1500}>
-              <p>iman-ai.tech()</p>
-            </Link>
-          </div>
-
-          <div className='navbar_links'>
-            <Link activeClass="active" className="links" to="about-container" spy smooth duration={1500}>
-              <p>.about()</p>
-            </Link>
-            <Link activeClass="active" className="links" to="skills-container" spy smooth duration={1500}>
-              <p>.skills()</p>
-            </Link>
-            <Link activeClass="active" className="links" to="projects-container" spy smooth duration={1500}>
-              <p>.projects()</p>
-            </Link>
-            <Link activeClass="active" className="links" to="contacts-container" spy smooth duration={1500}>
-              <p>.links()</p>
-            </Link>
-          </div>
-        </Toolbar>
+    <div id="navbar">
+      <Link activeClass="active" className="links" to="landing-container" spy smooth duration={1500}>
+        <p>.about()</p>
+      </Link>
+      <Link activeClass="active" className="links" to="skills-container" spy smooth duration={1500}>
+        <p>.skills()</p>
+      </Link>
+      <Link activeClass="active" className="links" to="projects-container" spy smooth duration={1500}>
+        <p>.projects()</p>
+      </Link>
+      <Link activeClass="active" className="links" to="contacts-container" spy smooth duration={1500}>
+        <p>.links()</p>
+      </Link>
     </div>
   );
 }
